ebd6cbe95f01450097bd92d44758e849228afe56
Žák Martin <zak_m@emm.sk>
2017-07-13 12:35:40 +0200

    breaking one component AppComponent into 2 (App, HeroDeail) + created Hero Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,6 @@ export class AppComponent {
     onSelect(hero: Hero): void {
       this.selectedHero = hero;
     }
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
